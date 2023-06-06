37a9ea621742bbfa801a4219792ff8e78884588d
ajbajbajb <36610573+ajbajbajb@users.noreply.github.com>
2023-06-06 13:45:37 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,18 @@
-import * as React from 'react'
-import { Link } from 'gatsby'
-import '../components/styles/styles.css'
+import * as React from "react";
+import { Link } from "gatsby";
+import "../components/styles/styles.css";
 
-const Layout = ({children}) => {
-
+const Layout = ({ children }) => {
   return (
     <>
-    <nav>
-      <Link to='/'><h4>Sentiment Slider Test V1.0</h4></Link>
-    </nav>
-    <main>
-      {children}
-    </main>
+      <nav>
+        <Link to="/">
+          <h4>Sentiment Slider Test V1.2</h4>
+        </Link>
+      </nav>
+      <main>{children}</main>
     </>
+  );
+};
 
-  )
-}
-
-export default Layout
+export default Layout;
